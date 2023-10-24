2904d686d3fdf5479261cab59ee8bde275107a8f
Yamkela <ncby9zfs7@vossie.net>
2023-10-25 01:13:53 +0200

    Launch

--- server/index.js
+++ server/index.js
@@ -31,14 +31,14 @@ app.use("/send-email",(req,res)=>{
   var client = new postmark.ServerClient("b12aa721-e892-4c55-a3e3-ebfb1732bc11");
 
 
-client.sendEmail({
+  client.sendEmail({
   "From": "ncby9zfs7@vossie.net",
   "To": "ncby9zfs7@vossie.net",
   "Subject": "Hello from Postmark",
   "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
   "TextBody": "Hello from Postmark!",
   "MessageStream": "notifications-1"
-});
+  });
   
 res.send(userInformation);
 
