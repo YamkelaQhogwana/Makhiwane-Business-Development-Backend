2252e05bab33bbf49579f10ef3e91f89aa7dc166
Yamkela <ncby9zfs7@vossie.net>
2023-10-24 23:51:21 +0200

    Commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ var postmark = require("postmark");
 const express = require("express");
 const app =express();
 const port = 5000;
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 
 app.listen((5000, (req,res)=>{
