8df0d8fb61cc5d53babd5d4ec96c4694506a4f4d
Yamkela <ncby9zfs7@vossie.net>
2023-10-25 01:56:24 +0200

    Invoice

--- server/index.js
+++ server/index.js
@@ -44,6 +44,157 @@ app.post("/send-email", (req, res) => {
       )
       .join('');
 
+  //Invoice Content 
+  const content = `
+      <html lang="en">
+  <body
+    style="
+      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+        'Lucida Sans', Arial, sans-serif;
+    "
+  >
+    <section
+      class="logo-and-slogan"
+      style="
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      "
+    >
+      <div class="image-container">
+        <img
+          src="https://i.ibb.co/2vtgW2Y/makhiwane-logo.png"
+          alt="makhiwane-logo"
+          style="height: 60px"
+        />
+      </div>
+      <p style="text-align: center">
+        MAKHIWANE BUSINESS DEVELOPMENT SOLUTIONS <br /><span
+          >Diversity leads to innovation culture</span
+        >
+      </p>
+    </section>
+    <section
+      class="invoice-details"
+      style="
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 0.5rem;
+        padding-top: 2rem;
+        border-top: 10px solid black;
+      "
+    >
+      <section class="business">
+        <h1 style="font-size: medium">
+          MAKHIWANE BUSINESS DEVELOPMENT <br />PROPRIETY LIMITED
+        </h1>
+        <h2 style="font-size: medium">REGISTRATION NUMBER (2023/947821/07)</h2>
+        <address style="font-style: normal; font-size: small">
+          17 Ferero Street <br />
+          Glenanda <br />
+          Johannesburg South <br />
+          Gauteng <br />
+          2091
+        </address>
+      </section>
+
+      <section
+        class="bill-to"
+        style="
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: flex-end;
+        "
+      >
+        <section class="bill-to-content">
+          <h2 style="font-size: medium; text-transform: uppercase;">INVOICE TO: <br />${userInformation.companyName}</h2>
+          <address
+            style="
+              font-style: normal;
+              font-size: small;
+            "
+          >
+            ${address} <br />
+            ${city} <br />
+            ${country} <br />
+            ${postalCode} <br />
+          </address>
+        </section>
+      </section>
+    </section>
+
+    <section
+      class="invoice-date-and-number"
+      style="
+        display: flex;
+        gap: 0;
+        flex-direction: column;
+        align-items: center;
+        padding-bottom: 0rem;
+        background-color: lightblue;
+        color: white;
+        color: black;
+      "
+    >
+      <p style="font-size: small; text-transform: uppercase;">
+        INVOICE NUMBER : ${invoiceNumber} <br />INVOICE DATE : ${date}
+      </p>
+    </section>
+
+    <section
+      class="services"
+      style="display: flex; flex-direction: column; gap: 0.5rem"
+    >
+      <article style="display: flex; justify-content: space-between">
+        <h3>SERVICE NAME</h3>
+        <h3>PRICE</h3>
+      </article>
+
+      <section
+        class="items"
+        style="
+          display: flex;
+          flex-direction: column;
+          padding-bottom: 0.3rem;
+          border-bottom: 3px solid orange;
+        "
+      >
+        ${articleHTML}
+      </section>
+
+      <section
+        class="total"
+        style="
+          display: flex;
+          justify-content: space-between;
+          border-bottom: 3px solid orange;
+        "
+      >
+        <h1>INVOICE TOTAL:</h1>
+        <h1>R${total}</h1>
+      </section>
+
+      <section
+        class="banking-details"
+        style="display: flex; align-items: center; justify-content: center"
+      >
+        <h3>BANKING DETAILS</h3>
+      </section>
+      <article style="padding-bottom: 2rem; border-bottom: 10px solid black">
+        Payment is due within 90 days <br />
+        Account details <br />
+        The account holder: MAKHIWANE BUSINESS DEVELOPMENT <br />
+        Bank Name : FNB <br />
+        Account Number : 63065723760 Branch Name : The Glen Mall Branch Code :
+        259605
+      </article>
+    </section>
+  </body>
+</html>
+    `;
+
 
   client.sendEmail(emailData, (error, result) => {
     if (error) {
