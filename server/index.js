af8c8a7a6fdd612cd4bed32768b5e4de1c215198
Yamkela <ncby9zfs7@vossie.net>
2023-10-25 15:31:23 +0200

    Fix Again

--- server/index.js
+++ server/index.js
@@ -1,35 +1,56 @@
-const postmark = require("postmark");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5000;
-const bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const myemail = 'yamkela.qhogwana@gmail.com'; 
 const mypassword = 'opvtoenlmdliyrip'; 
 
-//Create the transporter
+// Create the transporter
 const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: myemail,
-        pass: mypassword,
-      },
- });
+  service: 'gmail',
+  auth: {
+    user: myemail,
+    pass: mypassword,
+  },
+});
 
-app.post("/send-email", (req,res)=>{
-   const recipientEmail = req.body.recipient_email;
-   const userInformation = req.body.userInformation;
-   const { country, city, postalCode, address } = userInformation;
-   const cartItems = req.body.cartItems;
-   const total = req.body.total;
-   const date = req.body.date;
-   const invoiceNumber = req.body.invoiceNumber; 
+app.post("/send-email", (req, res) => {
+  const recipientEmail = req.body.recipient_email;
+  const userInformation = req.body.userInformation;
+  const { country, city, postalCode, address } = userInformation;
+  const cartItems = req.body.cartItems;
+  const total = req.body.total;
+  const date = req.body.date;
+  const invoiceNumber = req.body.invoiceNumber;
 
-   res.send("We got the data from our side")
-})
+  transporter.sendMail({
+    from: myemail,
+    to: recipientEmail,
+    subject: 'Your Invoice',
+    // Email body and attachments go here
+  }, (error, info) => {
+    if (error) {
+      console.error("Email error:", error);
+      res.status(500).send("Failed to send email");
+    } else {
+      console.log("Email sent:", info.response);
+      res.send("Email sent successfully");
+    }
+  });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
