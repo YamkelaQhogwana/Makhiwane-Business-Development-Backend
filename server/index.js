496413b622d925ce7c47a962d6562bdb9ae6edc4
Yamkela <ncby9zfs7@vossie.net>
2023-10-26 22:50:55 +0200

    Commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const puppeteer = require('puppeteer-core');
+const fs = require('fs'); // For file system operations
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/pdf', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const { url } = req.query;
-
-    if (!url) {
-      return res.status(400).json({ message: 'A ?url query-parameter is required' });
-    }
-
     const browser = await puppeteer.connect({
       browserWSEndpoint: `wss://chrome.browserless.io?token=1cd0f188-03ec-42cb-b866-762258c9002c`,
     });
 
     const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto(url);
+    await page.setContent('<html><body><strong>Hello Yamkela</strong></body></html>');
 
-    const pdfBuffer = await page.pdf();
+    const pdfPath = 'MakhiwaneInvoice.pdf';
+    await page.pdf({ path: pdfPath, format: 'A4' });
+
+    const pdfBuffer = fs.readFileSync(pdfPath);
     res.status(200).header('Content-Type', 'application/pdf').send(pdfBuffer);
 
     await browser.close();
